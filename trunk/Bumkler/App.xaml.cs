7c5ddbedd52518bc70151e69ab600c3c4171b237
sintez.pm <sintez.pm@5a1ac876-5175-0664-4e50-892b0078a31a>
2010-06-22 13:15:42 +0000

    Sample client

---
+++ trunk/Bumkler/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+
+namespace Bumkler
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnActivated(EventArgs e)
+        {
+            base.OnActivated(e);
+            
+            
+            //VClient vkClient = new VClient(59156, 1864792, "rXiF2EEuXubV8PT77PYW");
+            
+        }
+
+    }
+}
---
+++ trunk/Bumkler/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using mshtml;
+using Vkontakte;
+using Vkontakte.Activity;
+using Vkontakte.MethodResults;
+
+namespace Bumkler
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int userId = 1; // Replace with your ID
+        private int appId = 1; // Replace with your app id
+        private int settingsMask = 255;
+
+        private Uri url;
+        private SessionData sessionData;
+        private bool authenticated = false;
+        private IVkAdapter adapter;
+
+        public MainWindow()
+        {
+            url = new Uri(String.Format("http://vkontakte.ru/login.php?app={0}&layout=popup&type=browser&settings={1}", appId, settingsMask));
+            InitializeComponent();
+            Authenticate();   
+        }
+
+        private void Authenticate()
+        {
+            webBrowser.Navigated += webBrowser_Navigated;
+            webBrowser.Navigate(url);
+        }
+
+        private void GetActivity()
+        {
+            if(!authenticated || adapter == null)
+            {
+                MessageBox.Show("Not authenticated!");
+                return;
+            }
+            
+            var activity = new Activity(adapter);
+
+            var res = activity.Get();
+            if (res is ActivityResult)
+            {
+                MessageBox.Show((res as ActivityResult).Activity);
+            }
+            else if (res is ErrorResult)
+            {
+                MessageBox.Show((res as ErrorResult).ErrorCode.ToString() + "\n" + (res as ErrorResult).ErrorMessage);
+            }
+        }
+
+        SessionData reRequestSessionData(string cookiesString)
+        {
+            var req = WebRequest.Create(url) as HttpWebRequest;
+            var cookies = new CookieContainer();
+
+            
+            cookies.Add(new Cookie("remixchk", "5", "/", "vkontakte.ru"));
+            var r = new Regex("remixsid=(.+);*?");
+            var result = r.Match(cookiesString);
+            
+            var remixsid = result.Groups.Count > 0 ? result.Groups[1].Value.ToString() : "";
+            
+            cookies.Add(new Cookie("remixsid", remixsid, "", "vkontakte.ru"));
+
+            req.CookieContainer = cookies;
+
+            req.UseDefaultCredentials = true;
+
+            var resp = req.GetResponse();
+            SessionData s = Utils.ParseLogin(resp.ResponseUri);
+            return s;
+        }
+
+       
+        void webBrowser_Navigated(object sender, NavigationEventArgs e)
+        {
+            
+            
+            if (e.Uri.ToString().Contains("http://vkontakte.ru/api/login_success.html"))
+            {
+                MessageBox.Show("Authenticatin succeded.");
+                
+                sessionData = Utils.ParseLogin(e.Uri) ?? reRequestSessionData(((sender as WebBrowser).Document as IHTMLDocument2).cookie);
+
+                adapter = new VkAdapter(sessionData, userId, appId);
+                authenticated = true;
+               
+            }
+
+            else if (e.Uri.ToString().Contains("http://vkontakte.ru/api/login_failure.html"))
+            {
+                MessageBox.Show("Authenticatin failed!");
+                authenticated = false;
+               
+            }
+            webBrowser.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            GetActivity();
+        }
+    }
+}
