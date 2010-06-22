c696586bc7d83ff3b7ec6ba61b1e5b4210d2b0ff
sintez.pm <sintez.pm@5a1ac876-5175-0664-4e50-892b0078a31a>
2010-06-22 15:15:12 +0000

    fixes

---
+++ VKClient/MethodLibraryBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Vkontakte
+{
+    public class MethodLibraryBase
+    {
+        protected IVkAdapter Adapter;
+
+        protected MethodLibraryBase() {}
+        protected MethodLibraryBase(IVkAdapter vkAdapter)
+        {
+            this.Adapter = vkAdapter;
+        }
+    }
+}
---
+++ VKClient/Misc/UserSettingsResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Vkontakte.MethodResults;
+
+namespace Vkontakte.Misc
+{
+    public class UserSettingsResult:IMethodResult
+    {
+        public int Settings { get; set; }
+    }
+}
