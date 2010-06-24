e9584ff3de91c6ecf43a284c9d260fbf4d6cf2eb
sintez.pm <sintez.pm@5a1ac876-5175-0664-4e50-892b0078a31a>
2010-06-24 13:53:36 +0000

    user classes added

---
+++ trunk/VKClient/Misc/IsAppUserResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Vkontakte.MethodResults;
+
+namespace Vkontakte.Users
+{
+    /// <summary>
+    /// Установил ли пользователь себе приложение.
+    /// Данный класс возвращается методом <see cref="Users.IsAppUser"/>.
+    /// </summary>
+    class IsAppUserResult:IMethodResult
+    {
+        /// <summary>
+        /// Установил ли пользователь себе приложение.
+        /// </summary>
+        public bool AppInstalled { get; set; }
+    }
+}
